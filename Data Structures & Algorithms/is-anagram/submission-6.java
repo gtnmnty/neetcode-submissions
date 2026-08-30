4076d058773ab365aa9a2f4427aca3bd5dee5e46
Montyy <bgwhiskey64@gmail.com>
2026-08-30 20:20:14 +0800

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+
+        char[] array1 = s.toCharArray();
+        char[] array2 = t.toCharArray();
+
+        Arrays.sort(array1);
+        Arrays.sort(array2);
+        if(!Arrays.equals(array1, array2)){
+            return false;
+        }
+
+        return true;
+    }
+}
